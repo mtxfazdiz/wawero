2a28313fa839d73ef246899bbf63d5c47b1fcf65
mtxfazdiz <mtxfazdiz@o2.pl>
2017-12-23 20:40:19 +0100

    test

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Miody Wigor - '),
+    'name' => env('APP_NAME', 'Miody Wigor'),
 
     /*
     |--------------------------------------------------------------------------
