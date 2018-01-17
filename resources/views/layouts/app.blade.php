91e94333fe714e4ca37b4aa258decacc6e6d0cf3
mtxfazdiz <mtxfazdiz@o2.pl>
2018-01-17 20:43:36 +0100

    fff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
       <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light" id="myNavbar">
         <div id="myLogo" class="container">
           <a class="navbar-left" href="{{ url('/') }}">
-            <!-- <img>  src="pic/logo768.svg -->
+            <img class="logo" src="pic/logo.svg">  
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"  aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
