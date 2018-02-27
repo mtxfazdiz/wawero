f7a36ab92831b66893dc56bc9201790147b45b6a
mtxfazdiz <mtxfazdiz@o2.pl>
2018-02-27 15:48:55 +0100

    ssss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,7 @@
               <div class="carousel-caption text-left">
                 <h1>Miód jest pożywny!</h1>
                 <p>W 80% składa się z cukrów prostych: fruktozy i glukozy.</p>
-                <p><a class="btn btn-lg btn-outline-primary" href="#" role="button">Sign up today</a></p>
+                <p><a class="btn btn-lg btn-outline-primary" href="#" role="button">Sprawdź!</a></p>
               </div>
             </div>
           </div>
@@ -76,7 +76,7 @@
               <div class="carousel-caption text-left">
                 <h1>Miód jest smaczny!</h1>
                 <p>Nadaje niepowtarzalny smak potrawom.</p>
-                <p><a class="btn btn-lg btn-outline-primary" href="#" role="button">Learn more</a></p>
+                 <p><a class="btn btn-lg btn-outline-primary" href="#" role="button">Sprawdź!</a></p>
               </div>
             </div>
           </div>
@@ -87,7 +87,7 @@
               <div class="carousel-caption text-left">
                 <h1>Miód to naturalne lekarstwo!</h1>
                 <p>Zawiera bogaty zestaw witamin oraz makro i mikroelementów.</p>
-                <p><a class="btn btn-lg btn-outline-primary" href="#" role="button">Browse gallery</a></p>
+                <p><a class="btn btn-lg btn-outline-primary" href="#" role="button">Sprawdź!</a></p>
               </div>
             </div>
           </div>
@@ -117,22 +117,22 @@
         <!-- Three columns of text below the carousel -->
         <div class="row">
           <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <img class="rounded-circle" src="pic/miod_sample.jpg" alt="Miód" width="140">
+            <h2>Miód rzepakowy</h2>
+            <p>Miód rzepakowy uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Nasza oferta &raquo;</a></p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <img class="rounded-circle" src="pic/miod_sample.jpg" alt="Miód" width="140">
+            <h2>Miód lipowy</h2>
+            <p>Miód lipowy uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Nasza oferta &raquo;</a></p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <img class="rounded-circle" src="pic/miod_sample.jpg" alt="Miód" width="140">
+            <h2>Miód gryczany</h2>
+            <p>Miód gryczany uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Nasza oferta &raquo;</a></p>
           </div><!-- /.col-lg-4 -->
         </div><!-- /.row -->
 
@@ -143,7 +143,7 @@
 
         <div class="row featurette">
           <div class="col-md-7">
-            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+            <h2 class="featurette-heading">100 lat tradycji! <span class="text-muted">Produkujemy miód od 3 pokoleń.</span></h2>
             <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
           </div>
           <div class="col-md-5">
@@ -155,7 +155,7 @@
 
         <div class="row featurette">
           <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+            <h2 class="featurette-heading">Nasz miód jest rozlewany ręcznie. <span class="text-muted">See for yourself.</span></h2>
             <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
           </div>
           <div class="col-md-5 order-md-1">
@@ -194,8 +194,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <!-- <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script> -->
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
