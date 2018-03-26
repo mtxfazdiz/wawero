8c0e917d6d1933c10633c09ab1e32cd47d8a5a1c
mtxfazdiz <mtxfazdiz@o2.pl>
2018-03-26 21:56:01 +0200

    ssss

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -6,13 +6,38 @@
 
 @section('content')
 
+<div class="container marketing">
+	<div class="row paddTop15"></div>
+	<div class="row featurette">
+		<dic class="col-md-1"></dic>
+    	<div class="col-md-6 d-flex align-items-center">
 
-<div class="container">
-	<h2> kontakt</h2>
-	<br>
-	<br>
-	<br>
+    		<div class="card border-white" style="width: 30rem;">
+  				<ul class="list-group list-group-flush">
+    				<li class="list-group-item border-white"><h2>Kontakt</h2></li>
+    				<li class="list-group-item border-white">Gospodarstwo pasieczne "Miody Wigor"</li>
+    				<li class="list-group-item border-white">Rogity 3AB</li>
+    				<li class="list-group-item border-white">14-500 Braniewo</li>    				
+    				<li class="list-group-item border-white">Woj. Warmińsko-Mazurskie</li>
+    				<li class="list-group-item border-white"><h3>+48 730 816 661</h3></li>
+  				</ul>
+			</div>
+
+    	</div>
+    
+
+
+    <div class="col-md-4 d-flex align-items-center">
+    	<div class="card border-white" style="width: 30rem;">
+  			<ul class="list-group list-group-flush">
+    			<li class="list-group-item border-white"><img class="featurette-image" src="pic/qr.svg" alt="QR" width="300"></li>
+    			<li class="list-group-item border-white">Zeskanuj kod aby dodać kontakt</li>
+  			</ul>
+		</div>
+    </div>
+    <dic class="col-md-1"></dic>
+	</div>
+	<hr class="featurette-divider">
 </div>
-
 
 @endsection
--- resources/views/onas.blade.php
+++ resources/views/onas.blade.php
@@ -7,11 +7,20 @@
 @section('content')
 
 
-<div class="container">
-	<h2> o nas</h2>
-	<br>
-	<br>
-	<br>
+<div class="container marketing">
+	<div class="row paddTop15"></div>
+	<div class="row featurette">
+          <div class="col-md-6">
+            <h2 class="featurette-heading">O nas</h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-6">
+            <img class="featurette-image img-fluid mx-auto" src="pic/rodzice.jpg" alt="Rodzice">
+          </div>
+        </div>
+
+
+	<hr class="featurette-divider">
 </div>
 
 
