13691c52c1f2228ba96f78a9a9acf94df61a76e5
mtxfazdiz <mtxfazdiz@o2.pl>
2018-11-07 19:43:44 +0100

    added 2 new products

---
+++ resources/views/layouts/untitled.php
@@ -0,0 +1,166 @@
+<h2 class="featurette-heading">Miód lipowy</h2>
+
+	</br>
+	<div class="row">
+	
+
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/maly_gryczany.jpg" alt="Miód" width="200">
+            
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/sredni_gryczany.jpg" alt="Miód" width="200">
+            
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/duzy_rzepak.jpg" alt="Miód" width="200">
+            
+          </div><!-- /.col-lg-4 -->
+          
+         
+    </div><!-- /.row -->
+
+    <hr class="featurette-divider">
+
+
+
+  
+	<h2 class="featurette-heading">Miód wielokwiatowy</h2>
+
+	</br>
+	<div class="row">
+	
+
+	
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/maly_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded" src="pic/sredni_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded img-bottom" src="pic/duzy_rzepak.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          
+         <div class="col-md-7"> 
+            
+            <p class="lead">Miód lipowy uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+    	</div>
+    </div><!-- /.row -->
+
+    <hr class="featurette-divider">
+
+    
+    <h2 class="featurette-heading">Miód rzepakowy</h2>
+
+	</br>
+	<div class="row">
+	
+
+	
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/maly_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded" src="pic/sredni_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded img-bottom" src="pic/duzy_rzepak.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          
+         <div class="col-md-7"> 
+            
+            <p class="lead">Miód lipowy uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+    	</div>
+    </div><!-- /.row -->
+
+    <hr class="featurette-divider">
+
+
+    
+    <h2 class="featurette-heading">Miód gryczany</h2>
+
+	</br>
+	<div class="row">
+	
+
+	
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/maly_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded" src="pic/sredni_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded img-bottom" src="pic/duzy_rzepak.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          
+         <div class="col-md-7"> 
+            
+            <p class="lead">Miód lipowy uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+    	</div>
+    </div><!-- /.row -->
+
+    
+
+    <hr class="featurette-divider">
+
+
+ 
+    <h2 class="featurette-heading">Miód z pyłkiem kwiatowym</h2>
+
+	</br>
+	<div class="row">
+	
+
+	
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/maly_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded" src="pic/sredni_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded img-bottom" src="pic/duzy_rzepak.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          
+         <div class="col-md-7"> 
+            
+            <p class="lead">Miód lipowy uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+    	</div>
+    </div><!-- /.row -->
+
+    
+
+    <hr class="featurette-divider">
+
+
+
+    <h2 class="featurette-heading">Miód z propolisem</h2>
+
+	</br>
+	<div class="row">
+	
+
+	
+          <div class="col-md-4 img-container">
+            <img class="rounded img-bottom" src="pic/maly_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded" src="pic/sredni_gryczany.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          <div class="col-md-4">
+            <img class="rounded img-bottom" src="pic/duzy_rzepak.jpg" alt="Miód" width="200">
+          </div><!-- /.col-lg-4 -->
+          
+         <div class="col-md-7"> 
+            
+            <p class="lead">Miód lipowy uznawany jest przez wielu znawców za najlepszy w smaku miód. Wynika to z jego bogatej zawartości. Miód ten jest trudny w produkcji i zarazem bogaty w składniki wszechstronnie poprawiające nasze zdrowie.</p>
+    	</div>
+    </div><!-- /.row -->
+
+    
+
+    <hr class="featurette-divider">
+
+
--- resources/views/naszaoferta.blade.php
+++ resources/views/naszaoferta.blade.php
@@ -18,7 +18,7 @@ Nasza oferta &raquo;
 
 <div class="row paddTop15"></div> <!-- lipowy -->
 	<div class="row featurette">
-		<dic class="col-md-1"></dic>
+		<div class="col-md-1"></div>
         <div class="col-md-5">
         	<div class="card text-center border-white d-flex align-items-center">
   					<div class="card-header bg-white border-white">
@@ -73,14 +73,14 @@ Nasza oferta &raquo;
             		<p class="lead">Miód lipowy działa antyseptycznie i uspokajająco, stosowany w leczeniu chorób układu oddechowego. Wskazany przy zapaleniu oskrzeli i płuc, usówa kaszel, stosowany przy przeziębieniach, grypie, stanach zapalnych błon śluzowych.</p>	
     		</span>
     	</div>
-    	<dic class="col-md-1"></dic>
+    	<div class="col-md-1"></div>
 	</div> <!-- lipowy end -->
 
 	<hr class="featurette-divider">
 
 	<div class="row paddTop15"></div> <!-- wielokwiatowy -->
 	<div class="row featurette">
-		<dic class="col-md-1"></dic>
+		<div class="col-md-1"></div>
         <div class="col-md-5">
         	<div class="card text-center border-white d-flex align-items-center">
   					<div class="card-header bg-white border-white">
@@ -135,7 +135,7 @@ Nasza oferta &raquo;
             		<p class="lead">Miód wielokwiatowy znajduje zastosowanie w stanach przeziębienia, wzmacnia organizm przed infekcjami oraz łagodzi objawy alergii i kataru. Wspomaga pracę wątroby, zapobiega chorobom woreczka żółciowego oraz minimalizuje problemy z jelitami. Stosuje się go jako naturalny antybiotyk, hamujący rozwój niepożądanych bakterii.</p>	
     		</span>
     	</div>
-    	<dic class="col-md-1"></dic>
+    	<div class="col-md-1"></div>
 	</div> <!-- wielokwiatowy end -->
 
 	<hr class="featurette-divider">
@@ -143,7 +143,7 @@ Nasza oferta &raquo;
 
 	<div class="row paddTop15"></div> <!-- rzepakowy -->
 	<div class="row featurette">
-		<dic class="col-md-1"></dic>
+		<div class="col-md-1"></div>
         <div class="col-md-5">
         	<div class="card text-center border-white d-flex align-items-center">
   					<div class="card-header bg-white border-white">
@@ -198,7 +198,7 @@ Nasza oferta &raquo;
             		<p class="lead">Miód rzepakowy posiada wyraźny wpływ leczniczy w chorobach serca, zwłaszcza: wieńcowych (dusznica sercowa) działa leczniczo na niewydolność naczyniową. Cholina w nim zawarta usuwa tłuszcze wątroby i wzmaga działanie pęcherzyka żółciowego. Przyśpiesza leczenie wrzodów trawiennych żołądka i dwunastnicy.</p>	
     		</span>
     	</div>
-    	<dic class="col-md-1"></dic>
+    	<div class="col-md-1"></div>
 	</div> <!-- rzepakowy end -->
 
 	<hr class="featurette-divider">
@@ -206,7 +206,7 @@ Nasza oferta &raquo;
 
 	<div class="row paddTop15"></div> <!-- gryczany -->
 	<div class="row featurette">
-		<dic class="col-md-1"></dic>
+		<div class="col-md-1"></div>
         <div class="col-md-5">
         	<div class="card text-center border-white d-flex align-items-center">
   					<div class="card-header bg-white border-white">
@@ -249,7 +249,7 @@ Nasza oferta &raquo;
             		<p class="lead">Miód gryczany zwany "przedłużaczem życia", pomaga przy przyswajaniu witaminy C, reguluje przepustowość ścianek włosowatych naczyń krwionośnych, tym samym przeciwdziała miażdżycy. Zalecany w nerwicach i stanach wyczerpania psychicznego. Reguluje rytm pracy serca.</p>	
     		</span>
     	</div>
-    	<dic class="col-md-1"></dic>
+    	<div class="col-md-1"></div>
 	</div> <!-- gryczany end -->
 
 	<hr class="featurette-divider">
@@ -258,7 +258,7 @@ Nasza oferta &raquo;
 
 	<div class="row paddTop15"></div> <!-- pylek -->
 	<div class="row featurette">
-		<dic class="col-md-1"></dic>
+		<div class="col-md-1"></div>
         <div class="col-md-5">
         	<div class="card text-center border-white d-flex align-items-center">
   					<div class="card-header bg-white border-white">
@@ -301,7 +301,7 @@ Nasza oferta &raquo;
             		<p class="lead">Miód z pyłkiem kwiatowym jest niezwykle cennym źródłem składników odżywczych. Zawiera makro i mikro elementy (wapń, żelazo, magnez). Stanowi bogate źródło witamin B1, B2 oraz witaminy C. Wskazany jest przy leczeniu anemii. Spożywanie go wzmacnia organizm oraz podnosi jego odporność. Zalecany przy leczeniu między innymi dróg żółciowych, miażdżycy, w chorobie wrzodowej żołądka i dwunastnicy. Ma działanie przeciwbakteryjne.</p>	
     		</span>
     	</div>
-    	<dic class="col-md-1"></dic>
+    	<div class="col-md-1"></div>
 	</div> <!-- pylek end -->
 
 	<hr class="featurette-divider">
@@ -310,7 +310,7 @@ Nasza oferta &raquo;
 
 	<div class="row paddTop15"></div> <!-- propolis -->
 	<div class="row featurette">
-		<dic class="col-md-1"></dic>
+		<div class="col-md-1"></div>
         <div class="col-md-5">
         	<div class="card text-center border-white d-flex align-items-center">
   					<div class="card-header bg-white border-white">
@@ -353,10 +353,101 @@ Nasza oferta &raquo;
             		<p class="lead">Miód z propolisem działa wzmacniająco, odtruwająco oraz pomaga w leczeniu wątroby. Dzięki swoim właściwościom przeciwzapalnym stosowany jest w stanach zapalnych górnych i dolnych dróg oddechowych. Ma silne działanie przeciwbakteryjne i przeciwgrzybiczne.</p>	
     		</span>
     	</div>
-    	<dic class="col-md-1"></dic>
+    	<div class="col-md-1"></div>
 	</div> <!-- propolis end -->
 
 	<hr class="featurette-divider">
+
+
+
+
+  <div class="row paddTop15"></div> <!-- nektarowo-spadziowy -->
+    <div class="row featurette">
+      <div class="col-md-1"></div>
+        <div class="col-md-5">
+          <div class="card text-center border-white d-flex align-items-center">
+            <div class="card-header bg-white border-white">
+              <ul class="nav  card-header-pills border-white">
+                  <li class="nav-item active">
+                    <a class="nav-link" data-toggle="tab" role="tab" href="#nektar_sredni">Średni</a>
+                  </li>
+              </ul>
+            </div>
+
+            <div class="card-body tab-content" >
+              
+              <div role="tabpanel" class="tab-pane active" id="nektar_sredni">
+                <div class="card border-white" style="width: 200px;">
+                  <img class="card-img-top" src="pic/sredni_nektarowo_spadziowy.jpg" alt="Card image cap">
+                  <ul class="list-group list-group-flush border-white">
+                    <li class="list-group-item border-white">Waga: 400 g </li>
+                    <li class="list-group-item border-white"><b>Cena 20 zł</b></li>
+                  </ul>
+              </div>
+              </div>
+          </div>
+      </div>  
+
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+          <span>
+            <h2>Miód nektarowo - spadziowy</h2>
+              <p class="lead">Miód nektarowo - spadziowy jest stosowany w schorzeniach serca, układu krążenia, leczeniu dróg oddechowych, zaburzeniach trawienia i stanach zapalnych organizmu. Jest to produkt odrzywczy i odnawiający w stanach wyczerpania fizycznego i psychicznego organizmu.
+              </p>  
+          </span>
+      </div>
+    <div class="col-md-1"></div>
+  </div> <!-- propolis end -->
+
+  <hr class="featurette-divider">
+
+
+
+
+  <div class="row paddTop15"></div> <!-- pyłek kwiatowy -->
+    <div class="row featurette">
+      <div class="col-md-1"></div>
+        <div class="col-md-5">
+          <div class="card text-center border-white d-flex align-items-center">
+            <div class="card-header bg-white border-white">
+              <ul class="nav  card-header-pills border-white">
+                  <li class="nav-item active">
+                    <a class="nav-link" data-toggle="tab" role="tab" href="#pylek_kwiatowy_sredni">Średni</a>
+                  </li>
+              </ul>
+            </div>
+
+            <div class="card-body tab-content" >
+              
+              <div role="tabpanel" class="tab-pane active" id="pylek_kwiatowy_sredni">
+                <div class="card border-white" style="width: 200px;">
+                  <img class="card-img-top" src="pic/sredni_pylek_kwiatowy.jpg" alt="Card image cap">
+                  <ul class="list-group list-group-flush border-white">
+                    <li class="list-group-item border-white">Waga: 200 g </li>
+                    <li class="list-group-item border-white"><b>Cena 22 zł</b></li>
+                  </ul>
+              </div>
+              </div>
+          </div>
+      </div>  
+
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+          <span>
+            <h2>Pyłek kwiatowy</h2>
+              <p class="lead">Pyłek kwiatowy jest tak zwaną bombą witaminową. Uzupełnia dzienne racje żywnościowe w aminokwasy, biopierwiastki, witaminy. Wzmacnia apetyt, reguluje przemianę materii, wzmacnia organizm, regeneruje. Osłania tkankę wątrobową przed zatruciem substancjami toksycznymi, ułatwia wydalanie toksyn. Ułatwia odnawianie tkanki wątrobowej uszkodzonej innymi przewlekłymi chorobami, przyjmowaniem dużej ilości leków. Podnosi odporność organizmu oraz działa przeciwmiażdżycowo.
+              </p>  
+          </span>
+      </div>
+    <div class="col-md-1"></div>
+  </div> <!-- propolis end -->
+
+  <hr class="featurette-divider">
+
+
+
+
+
 </div>
 
 
