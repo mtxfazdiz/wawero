ae685ee7d3e56a9db013aced0265d8f4641a7912
mtxfazdiz <mtxfazdiz@o2.pl>
2019-03-27 21:57:55 +0100

    kontakt v2 start

--- resources/views/kontakt2.blade.php
+++ resources/views/kontakt2.blade.php
@@ -18,8 +18,36 @@
 	<div class="row paddTop100"></div>
 		<h2 class="featurette-heading">Kontakt</h2>
 		<h2 class="featurette-heading">+48 730 816 661</h2>
-
-
+		<hr class="featurette-divider">
+		<div class="card-group">
+  		<div class="card text-center border-white">
+  			<i class="fa fa-facebook-square" style="font-size:160px;color:rgb(150,150,150);"></i>
+    		<div class="card-body">
+      			<p class="card-text lead">Dołącz do nas na facebooku</p>
+    		</div>
+    		<div class="card-footer bg-white border-white">
+      			<p><a class="btn btn-outline-secondary" href="https://fb.com/miodywigor" role="button" target="_blank">Facebook &raquo;</a></p>
+    		</div>
+  		</div>
+  		<div class="card text-center border-white">
+  			<i class="	fa fa-instagram" style="font-size:160px;color:rgb(150,150,150);"></i>
+    		<div class="card-body">
+      			<p class="card-text lead">Zobacz nas na instagramie</p>
+    		</div>
+    		<div class="card-footer bg-white border-white">
+      			<p><a class="btn btn-outline-secondary" href="https://instagram.com/miody_wigor" role="button" target="_blank">Instagram &raquo;</a></p>
+    		</div>
+  		</div>
+  		<div class="card text-center border-white">
+    		<img class="featurette-image img-fluid mx-auto" src="pic/qr.svg" alt="QR" width="160">
+    		<div class="card-body">
+      			<p class="card-text lead">Zeskanuj kod aby dodać kontakt</p>
+    		</div>
+    		<div class="card-footer bg-white border-white">
+      			
+    		</div>
+  		</div>
+	</div>
 
 
 
