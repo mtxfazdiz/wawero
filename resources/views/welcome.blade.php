d92d8548ae8851dd19c6a67bb4ea55dbe8ad288d
mtxfazdiz <mtxfazdiz@o2.pl>
2019-03-27 20:10:45 +0100

    wspolpraca c-c

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,89 +10,89 @@ HOME &raquo;
 
 
 
-     
+
 
 <div class="container marketing">
 
 
   <div class="card-group">
-  <div class="card text-center border-white">
-    <img class="card-img-top mx-auto d-block widthCITW" src="{{ url('/') }}/pic/sredni_rzepak.jpg" alt="Card image cap" width="130">
-    <div class="card-body">
-      <h5 class="card-title">Miód rzepakowy</h5>
-      <p class="card-text lead">Posiada wyraźny wpływ leczniczy w chorobach serca, zwłaszcza: wieńcowych (dusznica sercowa) działa leczniczo na niewydolność naczyniową.</p>
+    <div class="card text-center border-white">
+      <img class="card-img-top mx-auto d-block widthCITW" src="{{ url('/') }}/pic/sredni_rzepak.jpg" alt="Card image cap" width="130">
+      <div class="card-body">
+        <h5 class="card-title">Miód rzepakowy</h5>
+        <p class="card-text lead">Posiada wyraźny wpływ leczniczy w chorobach serca, zwłaszcza: wieńcowych (dusznica sercowa) działa leczniczo na niewydolność naczyniową.</p>
+      </div>
+      <div class="card-footer bg-white border-white">
+        <p><a class="btn btn-outline-secondary" href="{{ url('/naszaoferta') }}" role="button">Nasza oferta &raquo;</a></p>
+      </div>
     </div>
-    <div class="card-footer bg-white border-white">
-      <p><a class="btn btn-outline-secondary" href="{{ url('/naszaoferta') }}" role="button">Nasza oferta &raquo;</a></p>
+    <div class="card text-center border-white">
+      <img class="card-img-top mx-auto d-block widthCITW" src="{{ url('/') }}/pic/sredni_lipa.jpg" alt="Card image cap" width="130">
+      <div class="card-body">
+        <h5 class="card-title">Miód lipowy</h5>
+        <p class="card-text lead">Działa antyseptycznie i uspokajająco, stosowany w leczeniu chorób układu oddechowego. Wskazany przy zapaleniu oskrzeli i płuc.</p>
+      </div>
+      <div class="card-footer bg-white border-white">
+        <p><a class="btn btn-outline-secondary" href="{{ url('/naszaoferta') }}" role="button">Nasza oferta &raquo;</a></p>
+      </div>
+    </div>
+    <div class="card text-center border-white">
+      <img class="card-img-top mx-auto d-block widthCITW" src="{{ url('/') }}/pic/sredni_gryczany.jpg" alt="Card image cap" width="130">
+      <div class="card-body">
+        <h5 class="card-title">Miód gryczany</h5>
+        <p class="card-text lead">Zwany "przedłużaczem życia", pomaga przy przyswajaniu witaminy C, oraz przeciwdziała miażdżycy.</p>
+      </div>
+      <div class="card-footer bg-white border-white">
+        <p><a class="btn btn-outline-secondary" href="{{ url('/naszaoferta') }}" role="button">Nasza oferta &raquo;</a></p>
+      </div>
     </div>
   </div>
-  <div class="card text-center border-white">
-    <img class="card-img-top mx-auto d-block widthCITW" src="{{ url('/') }}/pic/sredni_lipa.jpg" alt="Card image cap" width="130">
-    <div class="card-body">
-      <h5 class="card-title">Miód lipowy</h5>
-      <p class="card-text lead">Działa antyseptycznie i uspokajająco, stosowany w leczeniu chorób układu oddechowego. Wskazany przy zapaleniu oskrzeli i płuc.</p>
+
+
+
+
+
+  <!-- START THE FEATURETTES -->
+
+  <hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">100 lat tradycji! <span class="text-muted">Hodujemy pszczoły od 4 pokoleń.</span></h2>
+      <p class="lead">Rodzinna tradycja pszczelarska sięga 1905 roku. Nabytą od tego czasu wiedzę oraz doświadczenie przekazujemy z pokolenia na pokolenie.</p>
     </div>
-    <div class="card-footer bg-white border-white">
-      <p><a class="btn btn-outline-secondary" href="{{ url('/naszaoferta') }}" role="button">Nasza oferta &raquo;</a></p>
-    </div>
-  </div>
-  <div class="card text-center border-white">
-    <img class="card-img-top mx-auto d-block widthCITW" src="{{ url('/') }}/pic/sredni_gryczany.jpg" alt="Card image cap" width="130">
-    <div class="card-body">
-      <h5 class="card-title">Miód gryczany</h5>
-      <p class="card-text lead">Zwany "przedłużaczem życia", pomaga przy przyswajaniu witaminy C, oraz przeciwdziała miażdżycy.</p>
-    </div>
-    <div class="card-footer bg-white border-white">
-      <p><a class="btn btn-outline-secondary" href="{{ url('/naszaoferta') }}" role="button">Nasza oferta &raquo;</a></p>
+    <div class="col-md-5">
+      <img class="featurette-image img-fluid mx-auto" src="{{ url('/') }}/pic/rodzice.jpg" alt="Rodzice">
     </div>
   </div>
-</div>
 
+  <hr class="featurette-divider">
 
+  <div class="row featurette">
+    <div class="col-md-7 order-md-2">
+      <h2 class="featurette-heading">Pszczelarstwo to nasza pasja. <span class="text-muted">Pozyskujemy miód najwyższej jakości.</span></h2>
+      <p class="lead">Zamiłowanie i pasja do pszczół oraz zdobyte doświadczenie umożliwia nam pozyskiwanie miodów najwyższej jakości.</p>
+    </div>
+    <div class="col-md-5 order-md-1">
+      <img class="featurette-image img-fluid mx-auto" src="{{ url('/') }}/pic/beekeeper.jpg" alt="Beekeeper">
+    </div>
+  </div>
 
+  <hr class="featurette-divider">
 
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">Warmia i Mazury. <span class="text-muted">Zielone płuca Polski.</span></h2>
+      <p class="lead">Nasza pasieka znajduje się na terenie województwa Warmińsko - Mazurskiego zwanego zielonymi płucami Polski. </p>
+    </div>
+    <div class="col-md-5">
+      <img class="featurette-image img-fluid mx-auto" src="{{ url('/') }}/pic/3rd.jpg" alt="Bees">
+    </div>
+  </div>
 
-     <!-- START THE FEATURETTES -->
+  <hr class="featurette-divider">
 
-     	<hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">100 lat tradycji! <span class="text-muted">Hodujemy pszczoły od 4 pokoleń.</span></h2>
-            <p class="lead">Rodzinna tradycja pszczelarska sięga 1905 roku. Nabytą od tego czasu wiedzę oraz doświadczenie przekazujemy z pokolenia na pokolenie.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="{{ url('/') }}/pic/rodzice.jpg" alt="Rodzice">
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Pszczelarstwo to nasza pasja. <span class="text-muted">Pozyskujemy miód najwyższej jakości.</span></h2>
-            <p class="lead">Zamiłowanie i pasja do pszczół oraz zdobyte doświadczenie umożliwia nam pozyskiwanie miodów najwyższej jakości.</p>
-          </div>
-          <div class="col-md-5 order-md-1">
-            <img class="featurette-image img-fluid mx-auto" src="{{ url('/') }}/pic/beekeeper.jpg" alt="Beekeeper">
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">Warmia i Mazury. <span class="text-muted">Zielone płuca Polski.</span></h2>
-            <p class="lead">Nasza pasieka znajduje się na terenie województwa Warmińsko - Mazurskiego zwanego zielonymi płucami Polski. </p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="{{ url('/') }}/pic/3rd.jpg" alt="Bees">
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <!-- /END THE FEATURETTES -->
+  <!-- /END THE FEATURETTES -->
 
 </div><!-- /.container -->
 
--- resources/views/wspolpraca.blade.php
+++ resources/views/wspolpraca.blade.php
@@ -8,31 +8,29 @@ Współpraca &raquo;
 
 
 
-
+ 
 
      
 
-<div class="container marketing ">
+<div class="container marketing">
   <h2 class="featurette-heading"></h2>
-    <div class="row featurette">
-    <div class="col-md-5">
-        <img class="featurette-image img-fluid mx-auto" src="pic/3miodyNew.jpg" alt="Miody"> 
+  <div class="row featurette">
+    <div class="col-md-5 d-flex align-items-center">
+      <img class="featurette-image img-fluid mx-auto" src="pic/3miodyNew.jpg" alt="Miody"> 
     </div>
     <div class="col-md-7">
-      
-        <form action="{{ route('ofertaSend') }}" role="form" method="post">
+      <form action="{{ route('ofertaSend') }}" role="form" method="post">
         {{ csrf_field() }}  
-          <div class="form-group">
-            <h2 class="featurette-heading">Jesteś zainteresowany współpracą?</h2>
-            <p class="lead">Wypełnij i wyślij poniższy formularz aby otrzymać ofertę!</p>
-            <input type="email" name="email" class="form-control form-control" placeholder="wpisz e-mail" required>
-          </div>
-          <div class="form-group">
-            <input type="text" name="name" class="form-control form-control" placeholder="wpisz swoje imię" required>
-          </div>
-          <button type="submit" class="btn btn-sm btn-outline-secondary">Wyślij</button>
-        </form> 
-         
+        <div class="form-group">
+          <h2 class="featurette-heading">Jesteś zainteresowany współpracą?</h2>
+          <p class="lead">Wypełnij i wyślij poniższy formularz aby otrzymać ofertę!</p>
+          <input type="email" name="email" class="form-control form-control" placeholder="wpisz e-mail" required>
+        </div>
+        <div class="form-group">
+          <input type="text" name="name" class="form-control form-control" placeholder="wpisz swoje imię" required>
+        </div>
+        <button type="submit" class="btn btn-sm btn-outline-secondary">Wyślij</button>
+      </form> 
     </div>
   </div>
   <hr class="featurette-divider">
