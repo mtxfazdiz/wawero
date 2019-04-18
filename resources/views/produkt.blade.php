6acba7c50361fed162042a73cfacc6d79f104f4c
mtxfazdiz <mtxfazdiz@o2.pl>
2019-04-18 20:22:05 +0200

    product page engine works

--- resources/views/produkt.blade.php
+++ resources/views/produkt.blade.php
@@ -7,44 +7,84 @@
 
 
 @section('content')
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center ">
+
+
+
+
+
+<!-- <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center ">
   <div class="col-md-7 p-lg-5 mx-auto my-5 ">
     <h1 class="display-4 font-weight-normal">Miody z dodatkami</h1>
     <p class="card-text lead">Definiujemy smak miodów na nowo.</p>
     <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
   </div>
-  <img class="featurette-image img-fluid mx-auto" src="pic/3miodyProd.png" alt="Miody">
+  <img class="featurette-image img-fluid mx-auto" src="pic/m01.jpg" alt="Miody" style="height: 250px;">
 </div>
 
-<!-- <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-  <div class="col-md-7 p-lg-5  my-5 ">
+
+
+<div class="row paddTop50"></div>
+<div class="position-relative overflow-hidden pr-3 p-md-3  text-center bg-light ramka">
+<div class="card-deck">
+  <div class="card border-1">
+    
+    <div class="card-body">
+      <h2 class="card-title display-5">Uwielbiane przez dzieci</h2>
+      <p class="lead">Zachwycają świerzością smaku.</p>
+      <a href="#" class="card-link">Another link</a>
+      <img class="card-img-bottom card-product" src="pic/m04.jpg" alt="Card image cap">
+    </div>
+  
+  </div>
+  <div class="card border-1">
+
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="card-link">Another link</a>
+      <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
+    </div>
+    
+    
+  </div>
+</div>
+</div>
+
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="bg-light">
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
+  <div class="col-md-7 p-lg-5 mx-auto my-5 ">
     <h1 class="display-4 font-weight-normal">Miody z dodatkami</h1>
     <p class="card-text lead">Definiujemy smak miodów na nowo.</p>
     <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
   </div>
-  <img class="product-device shadow-sm d-none d-md-block" src="pic/3miodyProd.png" alt="Miody">
-</div>
- -->
-
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-  <div class=" mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-    <div class="my-3 p-3">
-      <h2 class="display-5">Uwielbiane przez dzieci</h2>
-      <p class="lead">Zachwycają świerzością.</p>
-    </div>
-    <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
-      <img  src="pic/ns.png" alt="Miody" style="height: 500px;">
-    </div>
-  </div>
-  <div class=" mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-    <div class="my-3 py-3">
-      <h2 class="display-5">Zupełnie nowe oblicze</h2>
-      <p class="lead">Malina, cynamon a może orzech włoski?</p>
-    </div>
-    <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
-      <img class="featurette-image img-fluid mx-auto" src="pic/ns.png" alt="Miody">
-    </div>
-  </div>
+  <img class="card-img-bottom card-product" src="pic/m01.jpg" alt="Miody" style="height: 250px;">
 </div>
 
 
@@ -53,68 +93,94 @@
 
 
 
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 bg-light">
+  <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 p-3">
       <h2 class="display-5">Uwielbiane przez dzieci</h2>
       <p class="lead">Zachwycają świerzością.</p>
       <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
     </div>
-    <img class="featurette-image img-fluid mx-auto" src="pic/ns.png" alt="Miody" style="height: 200px;">
-    <!-- <div class="bg-white box-shadow mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div> -->
+    <img class="card-img-bottom card-product mx-auto" src="pic/m04.jpg" alt="Card image cap">
   </div>
-  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+  <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 py-3">
       <h2 class="display-5">Zupełnie nowe oblicze</h2>
       <p class="lead">Malina, cynamon a może orzech włoski?</p>
       <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
     </div>
-    <img class="featurette-image img-fluid mx-auto" src="pic/ns.png" alt="Miody" style="height: 200px;">
+    <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
+   
   </div>
 </div>
 
 
 
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
   <div class="col-md-7 p-lg-5 mx-auto my-5 ">
     <h1 class="display-4 font-weight-normal">Miody odmianowe</h1>
     <p class="card-text lead">Dobrze znane a jednocześnie coraz lepsze.</p>
     <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
   </div>
   <!-- <div class="product-device box-shadow d-none d-md-block"></div> -->
-  <img class="featurette-image img-fluid mx-auto" src="pic/3miodyProd.png" alt="Miody" >
+  <img class="card-img-bottom card-product" src="pic/m01.jpg" alt="Miody" style="height: 250px;">
   <!-- <div class="product-device product-device-2 box-shadow d-none d-md-block"></div> -->
 </div>
 
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+
+
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 bg-light">
+  <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
+    <div class="my-3 p-3">
+      <h2 class="display-5">Idealny składnik twoich potraw</h2>
+      <p class="lead">Na codzień w twojej kuchni.</p>
+      <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
+    </div>
+    <img class="card-img-bottom card-product mx-auto" src="pic/m04.jpg" alt="Card image cap">
+  </div>
+  <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
+    <div class="my-3 py-3">
+      <h2 class="display-5">Zupełnie nowe oblicze</h2>
+      <p class="lead">To tylko niektóre z naszych propozycji!</p>
+      <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
+    </div>
+    <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
+   
+  </div>
+</div>
+
+<!-- <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
   <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
     <div class="my-3 p-3">
       <h2 class="display-5">Idealny składnik twoich potraw</h2>
       <p class="lead">Na codzień w twojej kuchni.</p>
     </div>
-    <img class="featurette-image img-fluid mx-auto" src="pic/ns.png" alt="Miody" style="height: 300px; border-radius: 21px 21px 0 0;">
-    <!-- <div class="bg-white box-shadow mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div> -->
+    <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
+    <div class="bg-white box-shadow mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
   </div>
   <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
     <div class="my-3 py-3">
       <h2 class="display-5">Another headline</h2>
       <p class="lead">To tylko niektóre z naszych propozycji!</p>
     </div>
-    <img class="featurette-image img-fluid mx-auto" src="pic/ns.png" alt="Miody" style="height: 300px; border-radius: 21px 21px 0 0;">
+    <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
   </div>
-</div>
+</div> -->
 
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center ">
+
+
+
+
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
   <div class="col-md-7 p-lg-5 mx-auto my-5 ">
     <h1 class="display-4 font-weight-normal">Każdy znajdzie miód dla siebie.</h1><br>
     <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
   </div>
 </div>
 
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-  <img class="featurette-image img-fluid mx-auto" src="pic/3miodyProd.png" alt="Miody" >
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
+  <img class="card-img-bottom card-product" src="pic/m01.jpg" alt="Miody" style="height: 250px;">
 </div>
 <div class="row paddTop100"></div>
-
+</div>
 
 @endsection
