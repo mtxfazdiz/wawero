bc0c4b16f2c76e3c29bfa7ccbc92e72e641e0df7
mtxfazdiz <mtxfazdiz@o2.pl>
2019-04-20 23:03:48 +0200

    ffff

--- resources/views/produkt.blade.php
+++ resources/views/produkt.blade.php
@@ -10,75 +10,8 @@
 
 
 
-
-
-<!-- <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center ">
-  <div class="col-md-7 p-lg-5 mx-auto my-5 ">
-    <h1 class="display-4 font-weight-normal">Miody z dodatkami</h1>
-    <p class="card-text lead">Definiujemy smak miodów na nowo.</p>
-    <a class="btn btn-outline-secondary" href="#">Sprawdź naszą ofertę</a>
-  </div>
-  <img class="featurette-image img-fluid mx-auto" src="pic/m01.jpg" alt="Miody" style="height: 250px;">
-</div>
-
-
-
-<div class="row paddTop50"></div>
-<div class="position-relative overflow-hidden pr-3 p-md-3  text-center bg-light ramka">
-<div class="card-deck">
-  <div class="card border-1">
-    
-    <div class="card-body">
-      <h2 class="card-title display-5">Uwielbiane przez dzieci</h2>
-      <p class="lead">Zachwycają świerzością smaku.</p>
-      <a href="#" class="card-link">Another link</a>
-      <img class="card-img-bottom card-product" src="pic/m04.jpg" alt="Card image cap">
-    </div>
-  
-  </div>
-  <div class="card border-1">
-
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="card-link">Another link</a>
-      <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
-    </div>
-    
-    
-  </div>
-</div>
-</div>
-
- -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <div class="bg-light">
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white mx-auto">
   <div class="col-md-7 p-lg-5 mx-auto my-5 ">
     <h1 class="display-4 font-weight-normal">Miody z dodatkami</h1>
     <p class="card-text lead">Definiujemy smak miodów na nowo.</p>
@@ -97,19 +30,18 @@
   <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 p-3">
       <h2 class="display-5">Uwielbiane przez dzieci</h2>
-      <p class="lead">Zachwycają świerzością.</p>
+      <p class="lead">Zachwycają barwą smaku.</p>
       <a class="btn btn-outline-secondary" href="{{ url('/naszaoferta/') }}">Sprawdź naszą ofertę</a>
     </div>
-    <img class="card-img-bottom card-product mx-auto" src="pic/m04.jpg" alt="Card image cap">
+    <img class="card-img-bottom card-product mx-auto" src="pic/m07.jpg" alt="Card image cap">
   </div>
   <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 py-3">
-      <h2 class="display-5">Zupełnie nowe oblicze</h2>
-      <p class="lead">Malina, cynamon a może orzech włoski?</p>
+      <h2 class="display-5">Mocno podkręcone.</h2>
+      <p class="lead">Więcej smaków. Więcej kolorów.</p>
       <a class="btn btn-outline-secondary" href="{{ url('/naszaoferta/') }}">Sprawdź naszą ofertę</a>
     </div>
-    <img class="card-product" src="pic/k/KwadratSredniPropolis.jpg" alt="Card image cap">
-   
+    <img class="card-img-bottom card-product mx-auto" src="pic/m09.jpg" alt="Card image cap">
   </div>
 </div>
 
@@ -121,9 +53,7 @@
     <p class="card-text lead">Dobrze znane a jednocześnie coraz lepsze.</p>
     <a class="btn btn-outline-secondary" href="{{ url('/naszaoferta/') }}">Sprawdź naszą ofertę</a>
   </div>
-  <!-- <div class="product-device box-shadow d-none d-md-block"></div> -->
   <img class=" card-product" src="pic/m06.jpg" alt="Miody" >
-  <!-- <div class="product-device product-device-2 box-shadow d-none d-md-block"></div> -->
 </div>
 
 
@@ -135,39 +65,18 @@
       <p class="lead">Na codzień w twojej kuchni.</p>
       <a class="btn btn-outline-secondary" href="{{ url('/naszaoferta/') }}">Sprawdź naszą ofertę</a>
     </div>
-    <img class=" card-product mx-auto" src="pic/k/KwadratSredniZPylkiem.jpg" alt="Card image cap">
+    <img class="card-img-bottom card-product mx-auto" src="pic/m10.jpg" alt="Card image cap">
   </div>
   <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 py-3">
-      <h2 class="display-5">Zupełnie nowe oblicze</h2>
-      <p class="lead">To tylko niektóre z naszych propozycji!</p>
+      <h2 class="display-5">Jakość to zdrowie.</h2>
+      <p class="lead">Wszystko czego potrzebujesz.</p>
       <a class="btn btn-outline-secondary" href="{{ url('/naszaoferta/') }}">Sprawdź naszą ofertę</a>
     </div>
-    <img class="card-img-bottom card-product" src="pic/m06.jpg" alt="Card image cap">
+    <img class="card-img-bottom card-product mx-auto" src="pic/m13.jpg" alt="Card image cap">
    
   </div>
 </div>
-
-<!-- <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-    <div class="my-3 p-3">
-      <h2 class="display-5">Idealny składnik twoich potraw</h2>
-      <p class="lead">Na codzień w twojej kuchni.</p>
-    </div>
-    <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
-    <div class="bg-white box-shadow mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-  </div>
-  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-    <div class="my-3 py-3">
-      <h2 class="display-5">Another headline</h2>
-      <p class="lead">To tylko niektóre z naszych propozycji!</p>
-    </div>
-    <img class="card-img-bottom card-product" src="pic/m05.jpg" alt="Card image cap">
-  </div>
-</div> -->
-
-
-
 
 
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
