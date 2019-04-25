09ec2d0a183007ccdd8abe5a4a3510f14f0f8bb1
mtxfazdiz <mtxfazdiz@o2.pl>
2019-04-25 21:17:57 +0200

    fff

--- resources/views/wspolpraca.blade.php
+++ resources/views/wspolpraca.blade.php
@@ -12,70 +12,48 @@ Współpraca &raquo;
     <div class="col-md-7 p-lg-5 mx-auto my-5 overflow-hidden text-center">
       <h1 class="display-4 font-weight-normal">Współpraca z Miody Wigor</h1>
       <p class="card-text lead">Dowiedz się dlaczego warto!</p>
+      
     </div>
-    <img class="card-img-bottom card-product mx-auto" src="pic/m04.jpg" alt="Miody">
+    <img class="card-img-bottom card-product mx-auto contain" src="pic/m06.jpg" alt="Miody" >
   </div>
-
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white mx-auto">
-  <div class="col-md-7 p-lg-5 mx-auto my-5 overflow-hidden text-center">
-    <h1 class="display-4 font-weight-normal">Miody z dodatkami</h1>
-    <p class="card-text lead">Definiujemy smak miodów na nowo.</p>
-    <a class="l" href="#form">Pobierz ofertę &raquo;</a>
-  </div>
-  <img class="card-img-bottom mx-auto card-product contain" src="pic/m06.jpg" alt="Miody">
-</div>
-
-
-
 
 
   <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 bg-light">
   <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 p-3">
-      <h2 class="display-5">Uwielbiane przez dzieci</h2>
-      <p class="lead">Zachwycają barwą smaku.</p>
+      <h2 class="display-5">Doświadczenie</h2>
+      <p class="lead">Zamiłowanie i pasja do pszczół oraz zdobyta<br>wiedza umożliwia nam pozyskiwanie<br>miodów najwyższej jakości.</p>
       <a class="l" href="#form">Pobierz ofertę &raquo;</a>
     </div>
-    <img class="card-img-bottom card-product mx-auto" src="pic/hiRes/m07.jpg" alt="Card image cap">
+    
   </div>
   <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 py-3">
-      <h2 class="display-5">Mocno podkręcone.</h2>
-      <p class="lead">Więcej smaków. Więcej kolorów.</p>
+      <h2 class="display-5">Wiarygodność</h2>
+      <p class="lead">Rodzinna tradycja pszczelarska sięga 1905 roku.<br>Nabytą od tego czasu wiedzę oraz doświadczenie<br>przekazujemy z pokolenia na pokolenie.</p>
       <a class="l" href="#form">Pobierz ofertę &raquo;</a>
     </div>
-    <img class="card-img-bottom card-product mx-auto" src="pic/hiRes/m09.jpg" alt="Card image cap">
+    
   </div>
 </div>
-
-
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
-  <div class="col-md-7 p-lg-5 mx-auto my-5 ">
-    <h1 class="display-4 font-weight-normal">Miody odmianowe</h1>
-    <p class="card-text lead">Dobrze znane a jednocześnie coraz lepsze.</p>
-    <a class="l" href="#form">Pobierz ofertę &raquo;</a>
-  </div>
-  <img class="card-img-bottom card-product mx-auto contain" src="pic/m06.jpg" alt="Miody" >
-</div>
-
 
 
 <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 bg-light">
   <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 p-3">
-      <h2 class="display-5">Idealny składnik twoich potraw</h2>
-      <p class="lead">Na codzień w twojej kuchni.</p>
+      <h2 class="display-5">Jakość</h2>
+      <p class="lead">Pozyskujemy produkty najwyższej klasy<br>jednocześnie dbając o wysoką jakość<br>świadczonych usług.</p>
       <a class="l" href="#form">Pobierz ofertę &raquo;</a>
     </div>
-    <img class="card-img-bottom card-product mx-auto" src="pic/hiRes/m10.jpg" alt="Card image cap">
+    
   </div>
   <div class=" mr-md-3 pt-3  pt-md-5 text-center overflow-hidden bg-white">
     <div class="my-3 py-3">
-      <h2 class="display-5">Jakość to zdrowie.</h2>
-      <p class="lead">Wszystko czego potrzebujesz.</p>
+      <h2 class="display-5">Lokalizacja</h2>
+      <p class="lead">Nasza pasieka znajduje się na terenie województwa<br> warmińsko - mazurskiego zwanego<br>zielonymi płucami Polski.</p>
       <a class="l" href="#form">Pobierz ofertę &raquo;</a>
     </div>
-    <img class="card-img-bottom card-product mx-auto" src="pic/hiRes/m13.jpg" alt="Card image cap">
+    
    
   </div>
 </div>
